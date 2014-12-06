d9a73cb02eff906e6b774e4dc5ef1d8d1f6b1984
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-06 17:47:13 +0000

    JavaScript-AJAX-JSON Basics : XMLHttpRequest

---
+++ public_html/script.js
@@ -0,0 +1,19 @@
+/* @ Stephen O'Connor */
+  (function() {
+     
+     /*
+      * XMLHttpRequest API available to web browser scripting languages such as JavaScript. 
+      * It is used to send HTTP or HTTPS requests to a web server and load
+      * the server response data back into the script
+      */
+      
+     // open connect and acces the data
+     var request = new XMLHttpRequest();
+     request.open('GET', 'data.txt', false);
+     request.send();
+     
+     if(request.status===200) {
+     console.log(request); 
+     document.writeln(request.responseText);
+  }
+  }());
