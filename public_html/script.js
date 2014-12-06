c0964cbffaacd76182df9af40603e355a81aa8ad
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-06 18:33:25 +0000

    Scripting to older browsers.

--- public_html/script.js
+++ public_html/script.js
@@ -5,16 +5,26 @@
       * XMLHttpRequest API available to web browser scripting languages such as JavaScript. 
       * It is used to send HTTP or HTTPS requests to a web server and load
       * the server response data back into the script
+      * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
       */
       
      // open connect and acces the data
-     var request = new XMLHttpRequest();
-     request.open('GET', 'data.txt', false);
-     request.send();
-     
-     // check request and write to page
-     if(request.status===200) {
-     console.log(request); 
-     document.writeln(request.responseText);
-  }
+     // change open method remove false 
+     // add event handler
+     // more compatible with older browsers
+     var request;
+     if(window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+     } else {
+        request = new ActiveXObject("Mircosoft.XMLHTTP");
+     }     
+     request.open('GET', 'data.txt');
+     request.onreadystatechange = function() {
+        if ((request.readyState === 4) && (request.status===200)) {
+           console.log(request);
+           document.writeln(request.responseText);
+        }
+     };
+     request.send();     
+
   }());
