6ff3c489e2510df04be53619ab26839f376e6979
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-07 05:13:02 +0000

    Parsing XML using AJAX

--- public_html/script.js
+++ public_html/script.js
@@ -18,17 +18,23 @@
      } else {
         request = new ActiveXObject("Mircosoft.XMLHTTP");
      }     
-     request.open('GET', 'data.txt');
+     request.open('GET', 'data.xml');
      request.onreadystatechange = function() {
         if ((request.readyState === 4) && 
                 (request.status===200)) {           
-           var modify = 
-                   document.getElementsByTagName('ul')
-           [1].getElementsByTagName('li');            
-           for(var i = 0; i < modify.length; i++) {
-              modify[i].innerHTML = 
-                      request.responseText;
+           console.log(request.responseXML.getElementsByTagName('to')[1].firstChild.nodeValue);
+           
+           var items =
+                   request.responseXML.getElementsByTagName('to');
+           var output = '<ul>';
+           for (var i = 0; i < items.length; i++) {
+              output += '<li>' +
+                      items[i].firstChild.nodeValue +
+                      '</li>';
            }
+           output += '</ul>';
+           
+           document.getElementById('update').innerHTML = output;
         }
      };
      request.send();     
