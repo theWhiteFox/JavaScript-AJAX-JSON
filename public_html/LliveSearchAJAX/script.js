9bbe33f1c5250409c492f1eadd6b4806bd148145
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-08 12:52:34 +0000

    Display information

--- public_html/LliveSearchAJAX/script.js
+++ public_html/LliveSearchAJAX/script.js
@@ -1,6 +1,19 @@
 /* @ Stephen O'Connor */
 (function() {
    
-
+$.getJSON('data.json', function(data) {   
+   // console.log(data); // test data loaded
+   var output = '<ul class="searchresults">';
+   $.each(data, function(key, val) {
+      output += '<li>';
+      output += '<h2>'+ val.name +'</h2>';
+      output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ 
+              val.name +'" />';
+      output += '<p>' + val.bio + '</p>';
+      output += '</li>';        
+   });
+   output += '</ul>';
+   $('#update').html(output);
+}); // get JSON
 
 }());
