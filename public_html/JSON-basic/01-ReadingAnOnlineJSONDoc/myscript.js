88e2ae379ba104d6eddcc605707f0947495a033b
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-11 10:59:14 +0000

    AJAX reading online JSON file - Datahandler()

---
+++ public_html/JSON-basic/01-ReadingAnOnlineJSONDoc/myscript.js
@@ -0,0 +1,22 @@
+/* @ Stephen O'Connor */
+(function () {
+         
+         // call the function in the JSON file
+         dataHandler = function(info){
+            
+         var output = '';
+         for (var i = 0; i <= info.links.length-1; i++) {
+            for (key in info.links[i]) {
+               if (info.links[i].hasOwnProperty(key)) {
+                  output += '<li>' +
+                          '<a href = "' + info.links[i][key] +
+                          '">' + key + '</a>';
+                  '</li>';
+               }
+            }
+         }
+         var update = document.getElementById('links');
+         update.innerHTML = output;
+      };
+
+}());
