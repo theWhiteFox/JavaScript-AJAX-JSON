37970ca081ee5d2532b394a8f688a4dbd8481396
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-25 15:58:27 +0000

    AJAX get, loop and display from XML file

---
+++ public_html/AJAX-basic/00-GetData/scriptAjax.js
@@ -0,0 +1,41 @@
+/* @ Stephen O'Connor */
+(function () {
+   getDataFromText = function () {
+      var xmlhttp;
+      if (window.XMLHttpRequest) {
+         xmlhttp = new XMLHttpRequest();
+      } else {
+         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            document.getElementById('data').innerHTML = xmlhttp.responseText;
+         }
+      };
+      xmlhttp.open("GET", "files/data.txt", true);
+      xmlhttp.send();
+   };
+   getDataFromXml = function () {
+      var xmlhttp;
+      if (window.XMLHttpRequest) {
+         xmlhttp = new XMLHttpRequest();
+      } else {
+         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            xmldata = xmlhttp.responseXML;
+            var films = xmldata.getElementsByTagName("film");
+            var output = '<ol>';
+            // output += films[0].getElementsByTagName('title')[0].firstChild.nodeValue;
+            for (var i = 0; i < films.length; i++) {
+               output += '<li>' + films[i].getElementsByTagName('title')[0].firstChild.nodeValue + '</li>';
+            }
+            output += '</ol>';
+            document.getElementById("data").innerHTML = output;
+         }
+      };
+      xmlhttp.open("GET", "files/films.xml", true);
+      xmlhttp.send();
+   };
+}());
